8918680fd02e4b07498da535daff528e4cdb03f5
Egor Rudman <rudman.14@mail.ru>
2022-11-24 15:27:00 +0300

    1

--- specs/My.spec.js
+++ specs/My.spec.js
@@ -1,14 +1,14 @@
-describe('pullandbeer', () => {
-    beforeEach(async () => {
-        await browser.url('https://www.pullandbear.com/')
-        await browser.setWindowSize(1280, 720)
-    })
+// describe('pullandbeer', () => {
+//     beforeEach(async () => {
+//         await browser.url('https://www.pullandbear.com/')
+//         await browser.setWindowSize(1280, 720)
+//     })
 
-    it('should display product', async () => {
-        await $('#continueButton').click()
-        await $('.c-genre-selector-item').click()
-        await $('//span[@class="c-main-nav-link-name" and contains(text(),"Обувь")]/parent::*/parent::a').click()
+//     it('should display product', async () => {
+//         await $('#continueButton').click()
+//         await $('.c-genre-selector-item').click()
+//         await $('//span[@class="c-main-nav-link-name" and contains(text(),"Обувь")]/parent::*/parent::a').click()
 
-        await expect((await $('//span[contains(text(), "Босоножки с ремешками, на каблуке")][1]'))).toExist()
-    });
-});
+//         await expect((await $('//span[contains(text(), "Босоножки с ремешками, на каблуке")][1]'))).toExist()
+//     });
+// });
--- specs/iCanWin.spec.js
+++ specs/iCanWin.spec.js
@@ -1,17 +1,17 @@
-// describe('pastebin', () => {
-//     const text = 'git config --global user.name  \"New Sheriff in Town\" \ngit reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\ngit push origin master --force'
-//     const title = 'how to gain dominance among developers'
+describe('pastebin', () => {
+    const text = 'git config --global user.name  \"New Sheriff in Town\" \ngit reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\ngit push origin master --force'
+    const title = 'how to gain dominance among developers'
     
-//     beforeEach(async () => {
-//         await browser.url('https://pastebin.com')
-//         await browser.setWindowSize(1280, 720)
-//     })
+    beforeEach(async () => {
+        await browser.url('https://pastebin.com')
+        await browser.setWindowSize(1280, 720)
+    })
     
-//     it('I Can Win', async () => {
-//         await $('#postform-text').setValue(text)
-//         await $('#postform-format').parentElement().$('.selection').click()
-//         await $('//li[text()="Bash"]').click()
-//         await $('#postform-name').setValue(title)
-//         await $('.btn.-big').click()
-//     });
-// });
+    it('I Can Win', async () => {
+        await $('#postform-text').setValue(text)
+        await $('#postform-format').parentElement().$('.selection').click()
+        await $('//li[text()="Bash"]').click()
+        await $('#postform-name').setValue(title)
+        await $('.btn.-big').click()
+    });
+});
