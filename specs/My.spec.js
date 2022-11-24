e85cf9813c14568666c90f5ccc34b419e9f25ad2
Egor Rudman <rudman.14@mail.ru>
2022-11-24 15:20:36 +0300

    junit

--- specs/My.spec.js
+++ specs/My.spec.js
@@ -8,7 +8,7 @@ describe('pullandbeer', () => {
         await $('#continueButton').click()
         await $('.c-genre-selector-item').click()
         await $('//span[@class="c-main-nav-link-name" and contains(text(),"Обувь")]/parent::*/parent::a').click()
-        
+
         await expect((await $('//span[contains(text(), "Босоножки с ремешками, на каблуке")][1]'))).toExist()
     });
 });
