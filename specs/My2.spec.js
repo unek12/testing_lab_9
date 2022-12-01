7604382a640a91331befd96f0c90dc4381115f9a
Egor Rudman <rudman.14@mail.ru>
2022-12-01 09:51:11 +0300

    added test

--- specs/My2.spec.js
+++ specs/My2.spec.js
@@ -8,27 +8,21 @@ describe('pullandbeer', () => {
     beforeEach(async () => {
         await browser.url('https://www.pullandbear.com/');
         await browser.setWindowSize(1280, 720);
+        await browser.deleteCookies()
     })
 
-    it('should display product', async () => {
-        await LocalizationPage.selectDefaultLocalization();
-        await GenderPage.selectGender();
-        await browser.pause(5000);
-        await Menu.clickNoveltiesMenuButton();
-
-        await expect(ProductPage.selectProduct('//span[contains(text(), "Длинное двубортное пальто")][1]')).toExist();
-     });
+    afterEach(async () => {
+    })
 
     it('should sort display product', async () => {
         await LocalizationPage.selectDefaultLocalization();
-        await browser.pause(5000);
+        await GenderPage.selectGender();
         await Menu.clickNoveltiesMenuButton();
 
         await expect(ProductPage.selectProduct('//span[contains(text(), "Длинное двубортное пальто")][1]')).toExist()
         await ProductPage.openSortMenu()
         await ProductPage.sortAsc()
         
-        await browser.deleteCookies()
         await expect(ProductPage.selectProduct('//span[contains(text(), "Серебристые серьги “Крест”")][1]')).toExist()
     });
 });
--- specs/objects/product_page.js
+++ specs/objects/product_page.js
@@ -1,8 +1,20 @@
 const Page = require("./page");
 
 class ProductPage extends Page {
+    get sortButton() {
+        return '//button[@class="filters-switch-display"][1]'
+    }
+
     async selectProduct(product) {
         return await $(product)
+    }
+
+    async openSortMenu(){
+        await $(this.sortButton).click()
+    }
+
+    async sortAsc(){
+        await $('//button[@id="sort-asc"][1]').click()
     }
 }
 
